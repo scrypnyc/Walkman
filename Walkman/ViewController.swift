045adc7223e4ebd653e9635abbe254d51d51d7e4
Alexander Skrypnyk <alexanderlookua@gmail.com>
2019-11-18 17:03:01 +0200

    Initial Commit

---
+++ Walkman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Walkman
+//
+//  Created by Alexander Skrypnyk on 18.11.2019.
+//  Copyright © 2019 Alexander Skrypnyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
