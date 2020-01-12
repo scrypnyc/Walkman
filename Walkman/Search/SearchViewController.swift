e7b57c5f0006fcf0b4fbf9a4a5a83811b0ea3840
Alexander Skrypnyk <alexanderlookua@gmail.com>
2020-01-12 17:38:29 +0200

    Update

--- Walkman/Search/SearchViewController.swift
+++ Walkman/Search/SearchViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 protocol SearchDisplayLogic: class {
-  func displayData(viewModel: Search.Model.ViewModel.ViewModelData)
+    func displayData(viewModel: Search.Model.ViewModel.ViewModelData)
 }
 
 class SearchViewController: UIViewController, SearchDisplayLogic {
@@ -21,24 +21,25 @@ class SearchViewController: UIViewController, SearchDisplayLogic {
     
     let searchController = UISearchController(searchResultsController: nil)
     private var searchViewModel = SearchViewModel.init(cells: [])
-  
-  // MARK: Setup
-  
-  private func setup() {
-    let viewController        = self
-    let interactor            = SearchInteractor()
-    let presenter             = SearchPresenter()
-    let router                = SearchRouter()
-    viewController.interactor = interactor
-    viewController.router     = router
-    interactor.presenter      = presenter
-    presenter.viewController  = viewController
-    router.viewController     = viewController
-  }
-  
-  
-  // MARK: View lifecycle
-  
+    private var timer: Timer?
+    
+    // MARK: Setup
+    
+    private func setup() {
+        let viewController        = self
+        let interactor            = SearchInteractor()
+        let presenter             = SearchPresenter()
+        let router                = SearchRouter()
+        viewController.interactor = interactor
+        viewController.router     = router
+        interactor.presenter      = presenter
+        presenter.viewController  = viewController
+        router.viewController     = viewController
+    }
+    
+    
+    // MARK: View lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
@@ -71,8 +72,8 @@ class SearchViewController: UIViewController, SearchDisplayLogic {
     }
 }
 
-    // MARK: UITableViewDelegate, UITableViewDataSource
-        
+// MARK: UITableViewDelegate, UITableViewDataSource
+
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return searchViewModel.cells.count
@@ -93,6 +94,11 @@ extension SearchViewController: UISearchBarDelegate {
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         print(searchText)
-        interactor?.makeRequest(request: Search.Model.Request.RequestType.getTracks(searchTerm: searchText))
+        
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { (_) in
+                        self.interactor?.makeRequest(request: Search.Model.Request.RequestType.getTracks(searchTerm: searchText))
+        })
     }
 }
+
